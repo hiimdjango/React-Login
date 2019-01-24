fcf2a353b03a7467d1ab7a4a645f7c9dedcbd760
Django <admin@Admins-MacBook-Pro.local>
2019-01-24 16:44:03 -0500

    Finishing the whole Sign up app and adjusting CSS

--- src/Authen.js
+++ src/Authen.js
@@ -13,7 +13,8 @@ var config = {
   
 class Authen extends Component {
     state = {
-        msg:''
+        msg:'',
+        isLoggedIn:false
     }
 
     //Authentification with Firebase and return Message ( this.state.msg)
@@ -25,21 +26,19 @@ class Authen extends Component {
 
         promise
         .then(user => {
-            //Show Logout button
-            var lout = document.getElementById('logout');
-            lout.classList.remove('hide');
             // Customise Msg with user email.
             let msg = 'Welcome back '+ email;
             const welcomeMsg = document.getElementById('msg');
             // Show msg in green
             welcomeMsg.classList.add('green');
-            this.setState({msg:msg});
+            this.setState({msg:msg,isLoggedIn:true});
         })
         promise
         .catch(e => {
             let err = e.message; //Get error message
             const errMsg = document.getElementById('msg');
             //Show message in Red
+            errMsg.classList.remove('green');
             errMsg.classList.add('red');
             this.setState({msg:err});
         });   
@@ -53,15 +52,15 @@ class Authen extends Component {
 
         promise
         .then(user => {
-            let msg='Welcome '+user.user.email; //Set custon msg
+            let msg='Welcome '+user.user.email +'! You can login with your new account!'; //Set custon msg
             //Add user to database 
             firebase.database().ref('users/'+user.user.uid).set({
                 email: email,
                 password: password
             });
             const welcomeMsg = document.getElementById('msg');
-            //Show Msg in Red
-            welcomeMsg.classList.add('red');
+            //Show Msg in Green
+            welcomeMsg.classList.add('green');
             this.setState({msg:msg});
         });
         promise
@@ -69,23 +68,29 @@ class Authen extends Component {
             let err=e.message; //Get error msg
             const errMsg = document.getElementById('msg');
             //Show Msg in red
+            errMsg.classList.remove('green');
             errMsg.classList.add('red');
             this.setState({msg:err});
         });
 
     }
 
-    //TODO Handle Signout function
+    signOut = () => {
+        firebase.auth().signOut();
+        this.setState({isLoggedIn:false,msg:'You have succesfuly logged Out'});
+    }
 
     render() {
         return(
             <div className='Authen'>
-                <input ref='email' id='email' type='email' placeholder='Email' /><br/>
-                <input ref='password' id='password' type='password' placeholder='Password' /><br/>
+                {!this.state.isLoggedIn ? <div>
+                    <input ref='email' id='email' type='email' placeholder='Email' /><br/>
+                    <input ref='password' id='password' type='password' placeholder='Password' /><br/>
+                </div> : null }   
                 <p id="msg">{this.state.msg}</p>
-                <button onClick={this.login}>Login</button>
-                <button onClick={this.signUp}>Sign up</button>
-                <button id="logout" className="hide">Logout</button>
+                {!this.state.isLoggedIn ? <button onClick={this.login}>Login</button> : null}
+                {!this.state.isLoggedIn ? <button onClick={this.signUp} className='register'>Sign up</button> : null}
+                {this.state.isLoggedIn ? <button onClick={this.signOut} id="logout" >Logout</button> : null}
             </div>
         )
     }
